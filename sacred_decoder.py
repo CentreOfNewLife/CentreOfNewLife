78b79af241d939cea84c191a959a4b26fa7455f8
CentreOfNewLife <taroln+FakeGithub@gmail.com>
2025-04-14 17:30:24 +0200

    Delete sacred_decoder.py

--- sacred_decoder.py
+++
@@ -1,9 +0,0 @@
-def decrypt():
-    key =  # The sacred number
-    encrypted = [1, 67, 68, 74, 79, 79, 10, 104, 79, 95, 69, 69]
-    message = ''.join([chr(c ^ key) for c in encrypted])
-    return message
-  
-if __name__ == "__main__":
-    print("Decoding the sacred message...")
-    print(":bulle_de_parole:", decrypt())  # Output: Kinder Bueno
